551479c23d49565c424cb2e6599e769ad01f02ef
Bart Barnard <b@mandarin.nl>
2018-08-12 21:51:31 +0200

    waarom die cv niet ook op github eigenlijk...?

---
+++ js/photo_data.js
@@ -0,0 +1,28 @@
+var photo_data = 
+{
+  "1":
+  {"foto":"waterhoenstraat.png",
+  "titel":"Zicht op de Waterhoenstraat",
+  "beschrijving": "Hier woon ik: een fijne doodlopende straat gelegen aan het Spoordok in Sneek. Een klein maar zeer fijn huisje, voorzien van een pelletkachel, een goeie netwerkverbinding, een piano en een bed. Veel meer heb je niet nodig eigenlijk."
+  },
+
+  "2":
+  {"foto":"lesgeven.png",
+  "titel":"Een actiefoto tijdens het lesgeven",
+  "beschrijving":"Het werken in het onderwijs is mijn passie. Hier ben ik bezig met een groepsbespreking van een studentenproject aan de Academie Minerva. Bij zo'n project ben ik meer een coach dan een docent – ik vertel niet hoe het zit of zou moeten zijn, maar ben een geïnteresseerde sparring-partner voor de groep (foto Arash Azad)." 
+  },
+
+  "3":
+  {"foto":"fietsen.png",
+   "titel":"Fietsen in Italië",
+   "beschrijving":"<p>In het eerste weekend van oktober wordt in de Chianti in Italië altijd een klassieke fietstocht gereden: tweehonderd kilometer op fietsen van minstens dertig jaar oud, over (grotendeels) onverharde wegen en met een klassieke sfeer. Hier kom ik net binnen in het kleine dorpje Aciano.</p><p>Ik heb overigens <a href='https://www.bartbarnard.nl/blog/?p=916'>een interessante blog</a> geschreven over mijn ervaringen hier.</p>"
+  },
+
+  "4":
+  {"foto":"hardlopen.jpg",
+   "titel":"Hardlopen in Groningen",
+   "beschrijving":"Elk jaar ren ik de vier mijl van Groningen, als onderdeel van een bedrijfsteam van ons instituut. De editie van 2017 was ik niet heel goed getraind, maar wist ik de afstand nog wel binnen het half uur af te leggen."
+  }
+
+}
+
